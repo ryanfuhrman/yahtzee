0f94e2ff9c0938d063b831e0f18d88da09db54ab
Ryan Fuhrman <rsfuhrman10@gmail.com>
2019-08-11 15:23:57 -0400

    Bug Fix: prevent user from scoring same rule multiple times

--- src/Game.js
+++ src/Game.js
@@ -60,12 +60,14 @@ class Game extends Component {
 
   doScore(rulename, ruleFn) {
     // evaluate this ruleFn with the dice and score this rulename
-    this.setState(st => ({
-      scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
-      rollsLeft: NUM_ROLLS,
-      locked: Array(NUM_DICE).fill(false),
-    }));
-    this.roll();
+    if (!this.state.scores[rulename]) {
+      this.setState(st => ({
+        scores: { ...st.scores, [rulename]: ruleFn(this.state.dice) },
+        rollsLeft: NUM_ROLLS,
+        locked: Array(NUM_DICE).fill(false),
+      }));
+      this.roll();
+    }
   }
 
   render() {
--- src/RuleRow.js
+++ src/RuleRow.js
@@ -1,14 +1,37 @@
-import React, { Component } from 'react';
-import './RuleRow.css'
+import React, { Component } from "react";
+import "./RuleRow.css";
 
 class RuleRow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      active: true,
+    };
+  }
+
+  disableRow = () => {
+    this.setState({
+      active: false,
+    });
+  };
+
+  onClick = () => {
+    this.disableRow();
+    this.props.doScore();
+  };
+
   render() {
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr
+        className={`RuleRow ${
+          this.state.active ? "RuleRow-active" : "RuleRow-disabled"
+        }`}
+        onClick={this.onClick}
+      >
         <td className="RuleRow-name">{this.props.name}</td>
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
-    )
+    );
   }
 }
 
